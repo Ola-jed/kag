401bf2a6f27164a8ab274ed2b5469bab4beef6cd
Ola-jed <olabijed@gmail.com>
2024-11-22 23:35:51 +0100

    feat: Reducing a grobner basis

--- lib/src/main/kotlin/core/algorithms/GrobnerBasisAlgorithms.kt
+++ lib/src/main/kotlin/core/algorithms/GrobnerBasisAlgorithms.kt
@@ -1,5 +1,26 @@
 package core.algorithms
 
-fun reduceBasis() {
-    TODO()
+import core.objects.Polynomial
+import core.operators.*
+
+fun <T : Number> minimizeBasis(grobnerBasis: List<Polynomial<T>>): List<Polynomial<T>> {
+    val newBasis = grobnerBasis.map { it / it.leadingCoefficient() }.toMutableList()
+
+    val toRemove = mutableSetOf<Int>()
+    val leadingTerms = newBasis.map(Polynomial<T>::leadingMonomial)
+    for (i in newBasis.indices) {
+        for (j in newBasis.indices) {
+            if (i != j && !toRemove.contains(j) && (leadingTerms[i] % leadingTerms[j]).exponents.isEmpty()) {
+                toRemove.add(i)
+                break
+            }
+        }
+    }
+
+    toRemove.sortedDescending().forEach(newBasis::removeAt)
+    for (i in newBasis.indices) {
+        newBasis[i] = newBasis[i] % newBasis.filterIndexed { j, _ -> j != i }
+    }
+
+    return newBasis
 }
--- sandbox/src/main/kotlin/Main.kt
+++ sandbox/src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
-import core.algorithms.buchberger
-import core.algorithms.f4
+import core.algorithms.minimizeBasis
 import core.objects.Polynomial
 import core.objects.PolynomialRing
+import core.order.CommonOrderings
 
 fun main() {
     val ring = PolynomialRing<Double>('x', 'y')
@@ -15,11 +15,48 @@ fun main() {
     val f = Polynomial<Double>(2.0 to x2, -4.0 to x, 1.0 to y2, -4.0 to y, 3.0 to one)
     val g = Polynomial<Double>(1.0 to x2, -2.0 to x, 3.0 to y2, -12.0 to y, 9.0 to one)
 
-    println("Buchberger")
-    val gb = buchberger(listOf(f, g)) // - 5/2 y² + 10y - 15/2
-    gb.forEach(::println)
 
-    val f4 = f4(listOf(f, g))
-    println("F4 Grobner basis")
-    f4.forEach(::println)
+    val gb = listOf(
+        // g1 = x^3 - 2xy
+        Polynomial(
+            1.0 to ring.monomial('x' to 3),
+            -2.0 to ring.monomial('x' to 1, 'y' to 1),
+            ordering = CommonOrderings.gradedLexicographicOrder()
+        ),
+        // g2 = x^2y - 2y^2 + x
+        Polynomial(
+            1.0 to ring.monomial('x' to 2, 'y' to 1),
+            -2.0 to ring.monomial('y' to 2),
+            1.0 to ring.monomial('x' to 1),
+            ordering = CommonOrderings.gradedLexicographicOrder()
+        ),
+        // g3 = x^2
+        Polynomial(
+            1.0 to ring.monomial('x' to 2),
+            ordering = CommonOrderings.gradedLexicographicOrder()
+        ),
+        // g4 = xy
+        Polynomial(
+            1.0 to ring.monomial('x' to 1, 'y' to 1),
+            ordering = CommonOrderings.gradedLexicographicOrder()
+        ),
+        // g5 = y^2 - (1/2)x
+        Polynomial(
+            1.0 to ring.monomial('y' to 2),
+            -0.5 to ring.monomial('x' to 1),
+            ordering = CommonOrderings.gradedLexicographicOrder()
+        )
+    )
+    println(gb)
+
+    println(minimizeBasis(gb))
+
+
+//    println("Buchberger")
+//    val gb = buchberger(listOf(f, g)) // - 5/2 y² + 10y - 15/2
+//    gb.forEach(::println)
+//
+//    val f4 = f4(listOf(f, g))
+//    println("F4 Grobner basis")
+//    f4.forEach(::println)
 }
